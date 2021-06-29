92252618da694734ce81991b14b343829c71bfa1
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-29 23:54:31 +0300

    Converting to async/await

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,70 +1,29 @@
 const express = require("express");
 const fs = require("fs");
-const { resolve } = require("path");
+const fsPromises = require("fs").promises;
 const datafile = "server/data/clothing.json";
 const router = express.Router();
 
 /* GET all clothing */
-router.route("/").get(function (req, res) {
-  /* fs.readFile(datafile, "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        let clothingData = JSON.parse(data);
-        console.log("Returning clothing data");
-        res.send(clothingData);
-      }
-    })
-    
-    console.log("Doing more work"); */
-
-  /* //using callbacks as a parameter
-   getClothingData((err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Returning clothing data");
-        res.send(data);
-      }
-    });
-    console.log("Doing more work"); */
-
-  getClothingData()
-    .then((data) => {
-      console.log("Returning clothing data to browser");
+router.route("/").get(async function (req, res) {
+  
+    try {
+      let data = await getClothingData();
+      console.log("Returning async data.");
       res.send(data);
-    })
-    .catch((err) => {
+    } catch (err) {
       res.status(500).send(err);
-    })
-    .finally(() => console.log("All done processing promise."));
-
-    console.log("Doing more work");
+    }
 });
 
-//using callbacks as a parameter
-/* function getClothingData(callback) {
-  fs.readFile(datafile, "utf8", (err, data) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      let clothingData = JSON.parse(data);
-      callback(null, clothingData)
-    }
-  })
-}   */
 
-function getClothingData() {
-  return new Promise((resolve, reject) => {
-    fs.readFile(datafile, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        let clothingData = JSON.parse(data);
-        resolve(clothingData);
-      }
-    });
-  });
+async function getClothingData() {
+  let rawData = await fsPromises.readFile(datafile, "utf-8")
+  let clothingData = JSON.parse(rawData);
+
+  console.log(clothingData);
+
+  return clothingData;
 }
 
 module.exports = router;
